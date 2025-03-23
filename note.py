d6114616b5688e92cc0341cd7c4e1275aec55d28
Igor Bujak <275435@student.pwr.edu.pl>
2025-03-23 16:06:54 +0100

    Added child class of usableItem, overriden use(), added scribble() and clear()

---
+++ note.py
@@ -0,0 +1,34 @@
+from usableItem import usableItem
+
+class note(usableItem):
+
+    __text = r"""
+               .--._.--.
+              ( O     O )
+              /   . .   \
+             .`._______.'.
+            /(           )\
+          _/  \  \   /  /  \_
+       .~   `  \  \ /  /  '   ~.
+      {    -.   \  V  /   .-    }
+    _ _`.    \  |  |  |  /    .'_ _
+    >_       _} |  |  | {_       _<
+     /. - ~ ,_-'  .^.  `-_, ~ - .\
+             '-'|/   \|`-`
+             """
+
+    def __init__(self, itemName, volume, weight):
+        super().__init__(itemName, volume, weight)
+
+    def use(self):
+        if(self.__text is not None):
+            print("Text of the note is:")
+            print(self.__text)
+        else:
+            print("The note is empty.")
+
+    def scribble(self, text):
+        self.__text = text
+
+    def clear(self):
+        self.__text = None
